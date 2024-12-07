d8907a5ae043991e777f95cc5ff155e2d02ea74d
student <student@K21-10-STUDENT>
2024-12-07 14:23:05 +0500

    Добавьте файлы проекта.

---
+++ Repair2/MenuWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Repair2
+{
+    /// <summary>
+    /// Логика взаимодействия для MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterOrder_Click(object sender, RoutedEventArgs e)
+        {
+            new NewOrderWindow().Show();    
+            this.Close();
+        }
+
+        private void EditOrder_Click(object sender, RoutedEventArgs e)
+        {
+            new EditOrderWindow().Show();
+            this.Close();
+        }
+
+        private void ProccesOrder_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
